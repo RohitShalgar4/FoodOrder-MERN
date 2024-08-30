13fba339a2f626fe55029d0666aad51963570430
RohitShalgar4 <rohishalgar04@gmail.com>
2024-08-30 10:42:39 +0530

    changes in logout(): if logout is success clear cart

--- frontend/src/actions/userAction.js
+++ frontend/src/actions/userAction.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { CLEAR_ERRORS, FORGOT_PASSWORD_FAIL, FORGOT_PASSWORD_REQUEST, FORGOT_PASSWORD_SUCCESS, LOAD_USER_FAIL, LOAD_USER_REQUEST, LOAD_USER_SUCCESS, LOGIN_FAIL, LOGIN_REQUEST, LOGIN_SUCCESS, LOGOUT_FAIL, LOGOUT_SUCCESS, NEW_PASSWORD_FAIL, NEW_PASSWORD_REQUEST, NEW_PASSWORD_SUCCESS, REGISTER_USER_FAIL, REGISTER_USER_REQUEST, REGISTER_USER_SUCCESS, UPDATE_PASSWORD_FAIL, UPDATE_PASSWORD_REQUEST, UPDATE_PASSWORD_SUCCESS, UPDATE_PROFILE_FAIL, UPDATE_PROFILE_REQUEST, UPDATE_PROFILE_SUCCESS } from "../constants/userConstant"
+import { CLEAR_CART } from "../constants/cartConstants";
 
 //Login
 export const login = (email, password) => async (dispatch) => {
@@ -100,6 +101,7 @@ export const logout = () => async (dispatch) => {
         dispatch({
             type: LOGOUT_SUCCESS,
         });
+        dispatch({ type: CLEAR_CART });
     } catch (error) {
         dispatch({
             type: LOGOUT_FAIL,
